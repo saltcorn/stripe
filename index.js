68001f173ec5b2bd8188988b3deffbbd73b23104
Tom Nielsen <tanielsen@gmail.com>
2021-01-27 08:55:55 +0000

    check session

--- index.js
+++ index.js
@@ -234,19 +234,23 @@ const success = (config, stripe) => {
     get_state_fields: () => [],
     run: async (table_id, viewname, view_cfg, state, { req }) => {
       const session_id = state.stripe_session_id;
-      const user_id = req.user.id;
-      // TODO: check session is completed
-      const stripe_session = await stripe.checkout.sessions.retrieve(
-        session_id
-      );
-      db.sql_log(stripe_session);
-      //elevate user
-      const user = await User.findOne({ id: user_id });
-      const session = user._attributes.stripe_sessions[session_id];
-      if (session && session.onsuccess && session.onsuccess.elevate_user_role)
-        await user.update({ role_id: session.onsuccess.elevate_user_role });
-      //say something nice
-      return "You're subscribed!";
+      const user_id = req.user && req.user.id;
+      if (session_id && user_id) {
+        // TODO: check session is completed
+        const stripe_session = await stripe.checkout.sessions.retrieve(
+          session_id
+        );
+        db.sql_log(stripe_session);
+        //elevate user
+        const user = await User.findOne({ id: user_id });
+        const session = user._attributes.stripe_sessions[session_id];
+        if (session && session.onsuccess && session.onsuccess.elevate_user_role)
+          await user.update({ role_id: session.onsuccess.elevate_user_role });
+        //say something nice
+        return "You're subscribed!";
+      } else {
+        return "No session";
+      }
     },
   };
 };
